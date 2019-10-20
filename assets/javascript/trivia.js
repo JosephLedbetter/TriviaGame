4b9354911f7a68e6bb67cdc15f972abd34730e39
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2019-10-19 20:14:07 -0400

    adding the questions for the multiple choice

---
+++ assets/javascript/trivia.js
@@ -0,0 +1,57 @@
+$(documnet).ready(function() {
+
+let triviaQuestions = [{ 
+    question: "What ghoul flies on a broom stick with a screeching cackle?",
+    lists: ["Swamp Monster", "Big Foot", "Black Cats", "Witches"],
+    answer: 3,
+}, {
+    question: "What ghoul resides in a snowy climate, larger than grizzly, and white all over?",
+    lists: ["Big Foot", "Swamp Monster", "Frankenstein", "Abominable Snowman"],
+    answer: 3,
+}, {
+    question: "This character is wrapped and buried, believe to live under the Pyramids of Egypt?",
+    lists: ["Bats", "Mummies", "Frankenstein", "Headless Horse"],
+    answer: 1,
+}, {
+    question: "What animal is black all over and believed to be bad luck?",
+    lists: ["Rat", "Cat", "Bat", "Snake"],
+    answer: 1,
+}, {
+    question: "Believed to be made of multiple men and brought back to life by an evil genius?",
+    lists: ["Abominable Snowman", "Big Foot", "Frankenstein", "Daffy Duck"],
+    answer: 2,
+}, {
+    question: "Which ghoul lives if responsible for UFO conspiracies?",
+    lists: ["Big Foot", "Alien", "Witch", "Cousin It"],
+    answer: 1,
+}, {
+    question: "Who is slimy, loves the water, and snatches people out of their boats?",
+    lists: ["Swamp Monster", "Mummy", "Witch", "Frankenstein"],
+    answer: 0,
+}, {
+    question: "He/She sleeps upside down, hypnotize beautiful damsels, and survives on drinking their blood?",
+    lists: ["Swamp Monster", "Frankenstein", "Dracula", "Elvis"],
+    answer: 2,
+}, {
+    question: "Screeching spirit that haunts cementaries?",
+    lists: ["Banshee", "Vampire", "Witch", "Zombie"],
+    answer: 0,
+}, {
+    question: "Loves human brains and only exist after being buried and coming back to life?",
+    lists: ["Vampire", "Ghost", "Zombie", "Werewolf"],
+    answer: 0
+}];
+
+// trivia multiple choice answer right || wrong
+let questionNumber;
+let rightAnswer;
+let wrongAnser;
+let blankAnswer;
+
+// timer & answer placeholders
+let seconds;
+let timer;
+let answers;
+let selectedAnswer;
+
+
